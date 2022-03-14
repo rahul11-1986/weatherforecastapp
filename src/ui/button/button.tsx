629dcd9f32a361786da682a5985e53b6695bcbf6
RAHUL RAJAN SINGH <43812633+rahul11-1986@users.noreply.github.com>
2022-03-14 22:10:29 +1100

    Added cursor to button

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -5,6 +5,7 @@ export const Button = styled.button`
 	flex: 1;
 	color: white;
 	border-right: 0;
+	cursor: pointer;
 	background: ${props => props.theme.primaryColor};
   	border-top: ${props =>  `1px solid ${props.theme.primaryColor}`};
   	border-bottom: ${props =>  `1px solid ${props.theme.primaryColor}`};
